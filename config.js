17df907e2b98a637a0925031d62b3e73e5be2776
dat nguyen <thedatnguyen20@gmail.com>
2022-07-05 14:50:00 +0700

    7/5/2022

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Tú Sương à!',
+    introDesc: `Nắng kia là của mặt trời
+    / Trông cậu lại giống nửa đời của tôi❤`,
+    btnIntro: 'Yêu ghê luôn ',
+    title: 'Phải chăng cậu đã yêu tớ ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Cậu mà không trả lời mà thoát là đồng ý nhé ',
+    btnYes: 'Vẫn cứ là thích Đạt ❤',
+    btnNo: 'Không, Đạt à đừng mơ',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Mình biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/dat230920/'
+}
